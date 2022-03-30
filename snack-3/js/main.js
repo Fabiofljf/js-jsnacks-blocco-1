78489a040ce32abcb1d650f3f0f840534cb20f42
FabioLaForesta <fabiolaforesta@icloud.com>
2022-03-30 12:04:31 +0200

    vorrei capire ...fondoooo

--- snack-3/js/main.js
+++ snack-3/js/main.js
@@ -1,16 +1,18 @@
 //Il software deve chiedere per 10 volte all’utente di inserire un numero.
 //Il programma stampa la somma di tutti i numeri inseriti.
 
+let numeriUtente;
+
 for (let i = 0; i < 10; i++) {
-    const numeriUtente = parseInt(prompt('inserisci un numero'))
+    numeriUtente = parseInt(prompt('inserisci un numero'))
 
     //stampo tutti i numeri
     console.log(numeriUtente)
 }
 
 //creo due categorie per sommare
-const totNumeriPari = (numeriUtente[i] % 2 == 0)
-const totNumeridispari = (numeriUtente[i] % 2 != 0)
+const totNumeriPari = (numeriUtente % 2 == 0)
+const totNumeridispari = (numeriUtente % 2 != 0)
 
 const numeroTot = totNumeriPari + totNumeridispari
 console.log(numeroTot)
