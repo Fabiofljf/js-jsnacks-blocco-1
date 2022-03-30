13233061365a71733d83603e574c05b28a1a48b7
FabioLaForesta <fabiolaforesta@icloud.com>
2022-03-30 11:50:46 +0200

    incarto

--- snack-3/js/main.js
+++ snack-3/js/main.js
@@ -1,9 +1,21 @@
 //Il software deve chiedere per 10 volte all’utente di inserire un numero.
+//Il programma stampa la somma di tutti i numeri inseriti.
 
-for (let i = 0; i <= 10; i++) {
-    const numeroUtente = Number(prompt('inserisci un numero'))
+for (let i = 0; i < 10; i++) {
+    const numeriUtente = parseInt(prompt('inserisci un numero'))
 
-    console.log(numeroUtente)
+    //stampo tutti i numeri
+    console.log(numeriUtente)
 }
 
-//Il programma stampa la somma di tutti i numeri inseriti.
+//creo due categorie per sommare
+if (numeriUtente[i] % 2 == 0) {
+    console.log('pari')
+}
+
+
+for (let i = 0; i < 10; i++) {
+    const numTot = numeriUtente[i] + numeriUtente[i]
+
+    console.log(numTot)
+}
