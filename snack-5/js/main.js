1aa0e54dec3c673d9da21535b1e60f79b6d7e112
FabioLaForesta <fabiolaforesta@icloud.com>
2022-03-30 12:55:20 +0200

    add js

---
+++ snack-5/js/main.js
@@ -0,0 +1,27 @@
+//Crea un array vuoto.
+const list = new Array()
+
+//Chiedi per 6 volte all’ utente di inserire un numero
+//variabile
+let confronta, aggiungi;
+confronta = 0
+
+
+for (let i = 0; i < 6; i++) {
+    const numeroUser = Number(prompt('inserisci un numero'))
+
+    if (numeroUser % 2 != 0) {
+        confronta == true
+        aggiungi == true
+    }
+
+    if (aggiungi == true) {
+        Element.push[numeroUser]
+        console.log(list)
+    }
+}
+
+
+//numeri dispari
+
+//se è dispari inseriscilo nell’ array.
