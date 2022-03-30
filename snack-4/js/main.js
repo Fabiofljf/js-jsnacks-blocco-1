ab7f930d10dc8d7b15959185b69e31f512f07a2b
FabioLaForesta <fabiolaforesta@icloud.com>
2022-03-30 12:22:39 +0200

    finish

---
+++ snack-4/js/main.js
@@ -0,0 +1,19 @@
+//In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+//Arrey con i nomi degli invitati
+const listaNomi = new Array('fabio', 'marco', 'peppe', 'lista', 'gino')
+
+//Chiedo il nome all'utente
+const nameUser = prompt('inserisci il tuo nome')
+
+//variabile true
+
+const entra = false
+
+//Verifico
+for (let i = 0; i <= listaNomi.length; i++) {
+    if (nameUser == listaNomi[i]) {
+        const entra = true
+        console.log('puoi entrare')
+    }
+}
