efbe44e545b848945297bfcae2ef47a9b2d43574
FabioLaForesta <fabiolaforesta@icloud.com>
2022-03-30 11:31:31 +0200

    finito

---
+++ snack-3/js/main.js
@@ -0,0 +1,9 @@
+//Il software deve chiedere per 10 volte all’utente di inserire un numero.
+
+for (let i = 0; i <= 10; i++) {
+    const numeroUtente = Number(prompt('inserisci un numero'))
+
+    console.log(numeroUtente)
+}
+
+//Il programma stampa la somma di tutti i numeri inseriti.
